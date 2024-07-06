06cb0fd12a556aa16dc522d979f1ba213a2ade2b
Dario Colucci <dario@colucci.dev>
2024-07-06 11:34:52 +0200

    dynamisch 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import BlogTeaser from "./components/Landingpage/BlogTeaser";
 import ShortLinks from "./components/Landingpage/ShortLinks";
 import PageContainer from "./components/PageContainer";
 
+export const dynamic = "force-dynamic";
 export default function Home() {
     return (
         <PageContainer image={`${process.env.NEXT_API}/assets/bmw_m8.png`}>
